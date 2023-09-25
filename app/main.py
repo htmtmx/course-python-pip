aac28bf4ab8a90ed7272284cc75c9f2253c30f0b
htmtmx <cesar.pineda@daksha.mx>
2023-09-25 17:09:44 -0600

    Using pandas in venv's

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-import utils, read_csv, charts
+import utils, read_csv, charts, pandas as pd
 
 def run():
-  data = read_csv.read_csv('./data.csv')
-  country = input('Type your country: ')
-  result = utils.population_by_country(data, country)
+  df = pd.read_csv('./data.csv') 
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country'].values
+  percentage = df['World Population Percentage'].values
+  charts.generate_bar_char('Africa', countries, percentage)
+  charts.generate_pie_chart('Africa', countries, percentage)
 
-  if len(result) > 0:
-    country = result[0]
-    labels, values = utils.get_population(country)
-    charts.generate_bar_char(country['Country'], labels, values)
-    charts.generate_pie_chart(country['Country'], labels, values)
+#   if len(result) > 0:
+#     country = result[0]
+#     labels, values = utils.get_population(country)
+#     charts.generate_bar_char(country['Country'], labels, values)
+#     charts.generate_pie_chart(country['Country'], labels, values)
 
 if __name__ == '__main__':
   run()
