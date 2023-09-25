fc09d91bf7a79073b7cdb112cf52cdb1123b1280
htmtmx <cesar.pineda@daksha.mx>
2023-09-25 17:25:21 -0600

    Using fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,37 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4, 5]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+            <html>
+                <head>
+                    <title>Contact</title>
+                </head>
+                <body>
+                    <h1>Contact</h1>
+                    <p>Phone: 123456789</p>
+                    <p>Email:
+                        <a href="mailto:mail@mail.com">
+                        </a>
+                        </p>
+                </body>
+            </html>
+        """
+
+
+@app.get('/categories')
+def get_categories():
+    return {'1': 'Category 1', '2': 'Category 2'}
 
 def run():
     store.get_categories()
