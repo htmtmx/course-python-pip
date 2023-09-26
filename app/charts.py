e124bc50b02818ae4eba472ebb35ab119bcd60f8
htmtmx <cesar.hpp96@hotmail.com>
2023-09-25 23:20:46 -0600

    File linking with docker

--- app/charts.py
+++ app/charts.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 def generate_bar_char(country, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig(f'./imgs/{country}_bar_chart.png')
+  plt.savefig(f'./imgs/{country}_bar_update.png')
 
 def generate_pie_chart(country, labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels = labels)
   ax.axis('equal')
-  plt.savefig(f'./imgs/{country}_pie_chart.png')
+  plt.savefig(f'./imgs/{country}_pie_update.png')
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
